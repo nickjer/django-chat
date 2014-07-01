bcdb2314eb7ff2e49c743e08c83691ff44b50559
nickjer <nickjer@hotmail.com>
2014-07-01 13:58:09 -0400

    latest_msgs added in MsgManager

--- chat/models.py
+++ chat/models.py
@@ -16,6 +16,22 @@ class AuthManager(models.Manager):
 
         return new_auth
 
+class MsgManager(models.Manager):
+    """
+    Custom manager for the ``Msg`` model.
+
+    The methods defined here provide shortcuts
+    to get latest msgs...
+    """
+    def latest_msgs(self, msg_id=0, num=10):
+        """
+        Get ``num`` latest msgs using id's greater
+        than ``msg_id``.
+        """
+        msg_query = Msg.objects.filter(id__gt=msg_id).order_by('-id')[:num]
+
+        return msg_query
+
 class Author(models.Model):
     name = models.CharField(max_length=25)
 
@@ -32,6 +48,8 @@ class Msg(models.Model):
     author = models.ForeignKey(Author)
     msg_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published',auto_now_add=True)
+
+    objects = MsgManager()
 
     def __str__(self):
         return self.msg_text
--- chat/views.py
+++ chat/views.py
@@ -18,20 +18,29 @@ def msgs(request):
         if form.is_valid(): # All validation rules pass (msg_id is int)
             m_id = form.cleaned_data['msg_id']
 
-            msg_w_auth = Msg.objects.filter(id__gt=m_id).order_by('-id').values('id', 'msg_text', 'author__name')[:10]
+            # Get 10 latest msgs since ``m_id``
+            msg_author = Msg.objects.latest_msgs(m_id, 10).values('id', 'msg_text', 'author__name')
     
-            msg_list = list(msg_w_auth) # Great way to populate cache
-            msg_list.reverse()
+            msg_list = list(msg_author) # Great way to populate cache
+            msg_list.reverse() # Necessary for output of msgs to be chronological
             try:
-                m_id = msg_list[-1]['id']
+                m_id = msg_list[-1]['id'] # Retrieve last msg id
             except (IndexError):
                 pass
+
+            # Record last msg id and list of msgs
             data = {'msg_id': m_id}
             data['msgs'] = msg_list 
     
+            # Format data into json
             data_json = json.dumps(data, indent=2)
+
+            # Output response as json content
             response = HttpResponse(data_json, content_type='text/json')
-            patch_cache_control(response, no_cache=True) #Make sure browser doesn't cache the request
+
+            # Make sure browser doesn't cache this request (not sure if needed)
+            patch_cache_control(response, no_cache=True)
+
             return response
         else:
             pass
