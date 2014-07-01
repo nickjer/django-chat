1d4ca013b1ea9bee54d38b08b6efdd8a23efa7f1
nickjer <nickjer@hotmail.com>
2014-07-01 12:45:00 -0400

    forms are now used for field validation

--- chat/forms.py
+++ chat/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
+class ListMsgsForm(forms.Form):
+    "Basic form for sending list of messages from last msg_id"
+    msg_id = forms.IntegerField(min_value=0)
+
 class MsgForm(forms.Form):
     "Basic form for accepting message from author"
-    author = forms.CharField(max_length=25)
+    name = forms.CharField(max_length=25)
     message = forms.CharField(max_length=200)
--- chat/views.py
+++ chat/views.py
@@ -4,49 +4,59 @@ from time import strftime
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
+from django.http import Http404
 from django.utils.cache import patch_cache_control
 from django.utils import timezone
 from django.utils.timezone import utc
 
 from chat.models import Msg, Author
+from chat.forms import MsgForm, ListMsgsForm
 
 class IndexView(generic.TemplateView):
     template_name = 'chat/index.html'
 
 def msgs(request):
-    try:
-        m_id = int(request.POST['msg_id'])
-    except (KeyError):
-        m_id = 0;
+    "List all messages in json format starting just after the last msg_id"
+    if request.method == 'POST': # Check if form has been submitted
+        form = ListMsgsForm(request.POST)
+        if form.is_valid(): # All validation rules pass (msg_id is int)
+            m_id = form.cleaned_data['msg_id']
 
-    msg_w_auth = Msg.objects.filter(id__gt=m_id).order_by('-id').values('id', 'msg_text', 'author__name')[:10]
+            msg_w_auth = Msg.objects.filter(id__gt=m_id).order_by('-id').values('id', 'msg_text', 'author__name')[:10]
     
-    msg_list = list(msg_w_auth)
-    msg_list.reverse()
-    try:
-        m_id = msg_list[-1]['id']
-    except (IndexError):
+            msg_list = list(msg_w_auth)
+            msg_list.reverse()
+            try:
+                m_id = msg_list[-1]['id']
+            except (IndexError):
+                pass
+            data = {'msg_id': m_id}
+            data['msgs'] = msg_list 
+    
+            data_json = json.dumps(data, indent=2)
+            response = HttpResponse(data_json, content_type='text/json')
+            patch_cache_control(response, no_cache=True) #Make sure browser doesn't cache the request
+            return response
+        else:
+            pass
+    else:
         pass
-    data = {'msg_id': m_id}
-    data['msgs'] = msg_list 
-    
-    data_json = json.dumps(data, indent=2)
-    response = HttpResponse(data_json, content_type='text/json')
-    patch_cache_control(response, no_cache=True) #Make sure browser doesn't cache the request
-    return response
+    raise Http404
 
 def send(request):
-    # Input the user and msg into database
-    try:
-        author_name = request.POST['name']
-        msg_text = request.POST['message']
-    except (KeyError):
-        return HttpResponse('', content_type='text/json')
+    "Input the user and msg into database then follow up by requesting latest msgs"
+    if request.method == 'POST': # Check if form has been submitted
+        form = MsgForm(request.POST)
+        if form.is_valid(): # All validation rules pass
+            author_name = form.cleaned_data['name']
+            msg_text = form.cleaned_data['message']
 
-    # Check if user already exists before creating
-    auth = Author.objects.get_or_create(name=author_name)[0]
-    auth.msg_set.create(msg_text=msg_text)
+            # Check if user already exists before creating
+            auth = Author.objects.get_or_create(name=author_name)[0]
+            auth.msg_set.create(msg_text=msg_text)
 
-    # Display msgs now
-    response = msgs(request)
-    return response
+            return msgs(request) # Display msgs
+        else:
+            return HttpResponse('', content_type='text/json') # Return nothing
+    else:
+        raise Http404
